5c27e5caf8f418c158a079d12c76de7157fec5ee
bokim <bo8902@gmail.com>
2024-06-20 19:41:09 +0900

    회원가입/로그인 페이지 추가

---
+++ src/main/java/com/chillin/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.chillin.repository;
+
+import com.chillin.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, UserQueryDSL{
+
+    @Override
+    <S extends User> S save(S entity);
+}
