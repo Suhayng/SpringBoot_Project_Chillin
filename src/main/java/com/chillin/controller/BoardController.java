0fd2de143c88e5f7a48d7fd67e0ed77c1ad664c4
choiyh <cyh010217@gmail.com>
2024-06-20 20:30:15 +0900

    에디터 돌아감 나이스

--- src/main/java/com/chillin/controller/BoardController.java
+++ src/main/java/com/chillin/controller/BoardController.java
@@ -3,13 +3,19 @@ package com.chillin.controller;
 import com.chillin.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,22 +29,41 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/editor_test")
-    public String editorTest(){
+    public String editorTest() {
         return "board/editor_test";
     }
 
     @PostMapping("/editor/image")
     @ResponseBody
-    public Map<String, Object> editorImage(@RequestParam("upload") MultipartFile image){
+    public Map<String, Object> editorImage(@RequestParam("upload") MultipartFile image) {
+
+        System.out.println(filePath);
         Map<String, Object> data = new HashMap<String, Object>();
-
-        if(image != null) {
-
-            data = boardService.fileUpload(filePath,image);
-
-            String imgPath = null;
+        if (image != null) {
+            data = boardService.fileUpload(filePath, image);
         }
         return data;
     }
+
+    @GetMapping("/getImage/{file_name}")
+    @ResponseBody
+    public ResponseEntity<byte[]> getImage(
+            @PathVariable(name = "file_name") String fileName){
+
+        InputStream in = null;
+        ResponseEntity<byte[]> entity = null;
+        fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8).replace("+","%20");
+        try{
+            in = new FileInputStream(filePath+"/"+fileName);
+            HttpHeaders headers=new HttpHeaders();
+            entity=new ResponseEntity<byte[]>(FileCopyUtils.copyToByteArray(in)
+                    ,headers,  HttpStatus.OK);
+        }catch (IOException e){
+            System.out.println("getImage 에서 에러"+e);
+            entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
+
 
 }
--- src/main/java/com/chillin/service/BoardServiceImpl.java
+++ src/main/java/com/chillin/service/BoardServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -16,22 +17,21 @@ public class BoardServiceImpl implements BoardService{
 
     @Override
     public Map<String, Object> fileUpload(String filePath, MultipartFile image) {
-        Map<String, Object> data = new HashMap<String, Object>();
+        Map<String, Object> data = new HashMap<>();
 
         if(image != null){
             String originalName = image.getOriginalFilename();
-            String fName = uploading(filePath,image);
+            String fileName = uploading(filePath,image);
 
             data.put("uploaded", 1);
-            data.put("fileName", originalName);
-            //data.put("url", "http://localhost:8123" + imgPath);
+            data.put("fileName", fileName);
+            data.put("url", "http://localhost:8080/getImage/" + fileName);
         }
 
         return data;
     }
 
     private String uploading(String filePath, MultipartFile image) {
-        File save = null;
 
         UUID uuid = UUID.randomUUID();
         String fileName = image.getOriginalFilename();
@@ -39,8 +39,16 @@ public class BoardServiceImpl implements BoardService{
         fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8)
                 .replace("+","%20");
 
-        System.out.println(fileName);
+        fileName = uuid + "_" + fileName;
 
-        return "any";
+        File save = new File(filePath,fileName);
+
+        try {
+                image.transferTo(save);
+        } catch (IOException e) {
+                save.delete();
+            throw new RuntimeException();
+        }
+        return fileName;
     }
 }
